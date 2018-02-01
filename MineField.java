1426527da445991b7fc76af0cb55013844d754bf
Rouhjp <roaming.new@gmail.com>
2018-02-02 02:41:28 +0900

    最適化,２次元配列をリストへ

--- MineField.java
+++ MineField.java
@@ -1,11 +1,11 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import java.util.function.Predicate;
 import java.util.stream.*;
 
 class MineField{
     private Status status = Status.READY;
-    private Cell[][] cells;
+    private List<Cell> cells = new ArrayList<>();
     private final int height;
     private final int width;
     private final int mines;
@@ -14,12 +14,7 @@ class MineField{
         this.height = height;
         this.width = width;
         this.mines = mines;
-        this.cells = new Cell[height][width];
-        for(int i = 0;i<height;i++){
-            for(int j = 0;j<width;j++){
-                this.cells[i][j] = new Cell();
-            }
-        }
+        IntStream.range(0, height*width).forEach(i->cells.add(new Cell(i)));
     }
     MineField(Difficulty d){
         this(d.height, d.width, d.mines);
@@ -42,83 +37,69 @@ class MineField{
     }
     private class Cell{
         private static final int MINE = 9;
-        private Face face = Face.DEFAULT;
+        private final int index;
         private int value = 0;
+        private Face face = Face.DEFAULT;
+        private Cell(int index){
+            this.index = index;
+        }
         private boolean isMine(){
             return this.value==MINE;
         }
         private void removeFace(){
             this.face = Face.REMOVED;
         }
+        private List<Cell> aroundCells(){
+            return IntStream.range(0, 9).filter(i->i!=4)
+                    .mapToObj(i->new int[]{index%width+i%3-1,index/width+i/3-1})
+                    .filter(p->p[0]>=0&&p[0]<width&&p[1]>=0&&p[1]<height)
+                    .mapToInt(p->p[0] + p[1]*width)
+                    .mapToObj(cells::get)
+                    .collect(Collectors.toList());
+        }
     }
     private enum Face{
         DEFAULT,FLAGGED,REMOVED
     }
-    private class Point{
-        private int x;
-        private int y;
-        private Point(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-        private boolean isWithin(){
-            return x>=0&&y>=0&&x<width&&y<height;
-        }
-        private Point[] getAroundPoints(){
-            return IntStream.range(0, 9).filter(i->i!=4).mapToObj(i->new Point(x+i%3-1, y+i/3-1))
-                    .filter(Point::isWithin).toArray(Point[]::new);
+    private void flagOpen(Cell target){
+        if(target.value==target.aroundCells().stream().filter(cell->cell.face==Face.FLAGGED).count()){
+            target.aroundCells().forEach(this::open);
         }
     }
-    private Cell getCellOf(Point point){
-        return cells[point.y][point.x];
-    }
-    private int countAroundCellOf(Point point, Predicate<Cell> check){
-        return (int)Arrays.stream(point.getAroundPoints()).map(this::getCellOf).filter(check).count();
-    }
-    private void flagOpen(Point point){
-        if(getCellOf(point).value==countAroundCellOf(point, cell->cell.face==Face.FLAGGED)){
-            Arrays.stream(point.getAroundPoints()).forEach(this::open);
-        }
-    }
-    private void toggleFlag(Point point){
-        Cell target =getCellOf(point);
+    private void toggleFlag(Cell target){
         if(target.face==Face.DEFAULT){
             target.face = Face.FLAGGED;
         }else if(target.face==Face.FLAGGED){
             target.face = Face.DEFAULT;
         }
     }
-    private void generate(Point point){
+    private void generate(Cell target){
         Random random = new Random(); // caution: this is LCG
         int count = 0;
         while(count<mines){
-            Point randomPoint = new Point(random.nextInt(width), random.nextInt(height));
-            if(Math.abs(randomPoint.x - point.x)>=2||Math.abs(randomPoint.y - point.y)>=2){
-                Cell target = getCellOf(randomPoint);
-                if(!target.isMine()){
-                    target.value = Cell.MINE;
+            int randomPoint = random.nextInt(height*width);
+            if(Math.abs(randomPoint%width - target.index%width)>=2||
+                    Math.abs(randomPoint/width - target.index/width)>=2){
+                Cell randomTarget = cells.get(randomPoint);
+                if(!randomTarget.isMine()){
+                    System.out.print(randomPoint+",");
+                    randomTarget.value = Cell.MINE;
                     count++;
                 }
             }
         }
-        for(int i = 0;i<height;i++){
-            for(int j = 0;j<width;j++){
-                if(!cells[i][j].isMine()){
-                    cells[i][j].value = countAroundCellOf(new Point(j, i), Cell::isMine);
-                }
-            }
-        }
+        cells.stream().filter(cell->!cell.isMine())
+                .forEach(cell->cell.value = (int)cell.aroundCells().stream().filter(Cell::isMine).count());
         this.status = Status.GENERATED;
     }
-    private void open(Point point){
-        Cell target = getCellOf(point);
+    private void open(Cell target){
         if(target.face==Face.DEFAULT){
             target.removeFace();
             if(++openCount==height*width-mines){
                 this.status = Status.SECURED;
             }else{
                 if(target.value==0){
-                    Arrays.stream(point.getAroundPoints()).forEach(this::open);
+                    target.aroundCells().forEach(this::open);
                 }else if(target.isMine()){
                     this.status = Status.EXPLODED;
                 }
@@ -129,8 +110,7 @@ class MineField{
     public enum CellView{
         M0,M1,M2,M3,M4,M5,M6,M7,M8,MINE,COVERED, FLAGGED, STILL_COVERED, MISS_FLAGGED
     }
-    private CellView getViewOf(Point point){
-        Cell target = getCellOf(point);
+    private CellView getViewOf(Cell target){
         if(isFinished()){
             switch(target.face){
                 case DEFAULT: return target.isMine()? CellView.STILL_COVERED: CellView.COVERED;
@@ -147,7 +127,7 @@ class MineField{
         throw new IllegalArgumentException();
     }
     CellView getViewOf(int x, int y){
-        return getViewOf(new Point(x, y));
+        return getViewOf(cells.get(x + y*width));
     }
     CellView[][] getView(){
         CellView[][] view = new CellView[height][width];
@@ -161,15 +141,15 @@ class MineField{
     @SuppressWarnings("fallthrough")
     void open(int x, int y){
         switch(status){
-            case READY: generate(new Point(x, y));
-            case GENERATED: open(new Point(x, y));
+            case READY: generate(cells.get(x + y*width));
+            case GENERATED: open(cells.get(x + y*width));
         }
     }
     void toggleFlag(int x, int y){
-        if(status!=Status.EXPLODED) toggleFlag(new Point(x, y));
+        if(status!=Status.EXPLODED) toggleFlag(cells.get(x + y*width));
     }
     void flagOpen(int x, int y){
-        if(status==Status.GENERATED) flagOpen(new Point(x, y));
+        if(status==Status.GENERATED) flagOpen(cells.get(x + y*width));
     }
     boolean isFinished(){
         return status==Status.EXPLODED||status==Status.SECURED;
@@ -190,6 +170,6 @@ class MineField{
         return mines;
     }
     int getMineCount(){
-        return mines - (int)Arrays.stream(cells).flatMap(Arrays::stream).filter(cell->cell.face==Face.FLAGGED).count();
+        return mines - (int)cells.stream().filter(cell->cell.face==Face.FLAGGED).count();
     }
 }
